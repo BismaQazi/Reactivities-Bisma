63b82698e3491169c3aa90d9ad8de2f5d12b74e9
BismaQazi <bisma.12.bss@gmail.com>
2022-09-14 11:17:37 -0700

    End of section #

--- API/Startup.cs
+++ API/Startup.cs
@@ -14,7 +14,9 @@ using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Persistence;
 using Microsoft.EntityFrameworkCore;
-
+using MediatR;
+using Application.Activities;
+using Application.Core;
 
 namespace API
 {
@@ -50,6 +52,8 @@ namespace API
                     policy.AllowAnyMethod().AllowAnyHeader().AllowCredentials().WithOrigins("http://localhost:3000");
                 });
             });
+            services.AddMediatR(typeof(List.Handler).Assembly);
+            services.AddAutoMapper(typeof(MappingProfiles).Assembly);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
